4cf924b07c953bea65ab322287dcc05683bf9f7b
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-07-14 21:55:12 -0300

    fix: Inversão de dependências para hash de senhas

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,17 @@
 import AppError from '@shared/errors/AppError';
 import FakerUsersRepository from '../repositories/fakes/FakerUsersRepository';
+import FakeHasHhProvider from '../providers/HashProvider/fakes/FakeHasHhProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakerUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHasHhProvider = new FakeHasHhProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHasHhProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -18,7 +24,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakerUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHasHhProvider = new FakeHasHhProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHasHhProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
