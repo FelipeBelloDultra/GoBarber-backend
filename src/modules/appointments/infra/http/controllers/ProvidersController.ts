52618ee5775046da2bbb93f77313019cb1d7afc8
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-07-24 20:13:53 -0300

    fix: Arrumando parâmetros

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -5,12 +5,12 @@ import ListProvidersService from '@modules/appointments/services/ListProvidersSe
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { id } = request.user;
+    const user_id = request.user.id;
 
     const listProviders = container.resolve(ListProvidersService);
 
     const providers = await listProviders.execute({
-      user_id: id,
+      user_id,
     });
 
     return response.json(providers);
