4c4513c4342be0784be00b0ff534e25a3902e269
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-07-25 23:30:26 -0300

    fix: Rate limit para limite de requisições

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import redis from 'redis';
+
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimit',
+  points: 5,
+  duration: 1,
+});
+
+async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Too many requests.', 429);
+  }
+}
+
+export default rateLimiter;
