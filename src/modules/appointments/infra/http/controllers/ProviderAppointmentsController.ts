e09546857f3c8309d4e2b92cda575948c4e1beee
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-07-28 17:58:34 -0300

    fix: Query params em controllers index

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -5,8 +5,8 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { day, month, year } = request.body;
     const provider_id = request.user.id;
+    const { day, month, year } = request.query;
 
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
@@ -14,9 +14,9 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
